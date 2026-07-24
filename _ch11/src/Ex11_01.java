93600bb417a8550dec1f920f64f896879325a893
monolail <qwe4229093@gmail.com>
2026-07-24 18:42:16 +0900

    docs & feat: _ch11 컬렉션 프레임워크 실습 코드 및 README 추가

---
+++ _ch11/src/Ex11_01.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Ex11_01 {
+    public static void main(String[] args) throws Exception {
+        // 기본길이가 10인 ArrayList생성.
+        ArrayList list1 = new ArrayList(10);
+        list1.add(new Integer(5)); // Auto Boxing 자동 수행.
+        list1.add(new Integer(2));
+        list1.add(new Integer(0));
+        list1.add(new Integer(1));
+        list1.add(new Integer(3));
+        ArrayList list2 = new ArrayList(10);
+        
+        
+        System.out.println(list1);
+        System.out.println(list2);
+        
+        // Collection은 인터페이스, Collections는 유틸 클래스
+        Collections.sort(list1);
+        Collections.sort(list2);
+        
+        System.out.println("정렬 후 list1");
+        System.out.println(list1);
+
+        // indexOf 인덱스 위치 알아내기
+        System.out.println(list1.indexOf(3));
+        // 없다면 -1을 출력.
+        System.out.println(list1.indexOf(4));
+
+        // remove삭제
+        list1.remove(4); // 값이 아닌 인덱스의 객체가 삭제
+        System.out.println(list1); 
+    }
+}
